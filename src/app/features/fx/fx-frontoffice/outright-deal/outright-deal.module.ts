eeff9cedb9aa44441d3535cf9f8cdbd5568d77bf
AyeshahKhalid <ayesha@kodevglobal.com>
2024-12-19 07:00:05 +0500

    outright screen new design

--- src/app/features/fx/fx-frontoffice/outright-deal/outright-deal.module.ts
+++ src/app/features/fx/fx-frontoffice/outright-deal/outright-deal.module.ts
@@ -9,6 +9,20 @@ import { OutrightAmendmentComponent } from './outright-amendment/outright-amendm
 import { OutrightCancellationComponent } from './outright-cancellation/outright-cancellation.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 
+import { HttpClientModule } from '@angular/common/http';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+// import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
+import { AngularEditorModule } from '@kolkov/angular-editor';
+import { ArchwizardModule } from 'angular-archwizard';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ToastrModule, ToastrService } from 'ngx-toastr';
+import { DropzoneModule, DropzoneConfigInterface, DROPZONE_CONFIG } from 'ngx-dropzone-wrapper';
+import { HighlightModule, HIGHLIGHT_OPTIONS } from 'ngx-highlightjs';
+import { NgxEditorModule } from 'ngx-editor';
+import { NgxMatIntlTelInputComponent } from 'ngx-mat-intl-tel-input';
+import { MaterialModuleModule } from 'src/app/materialModule/material-module/material-module.module';
+import { QuillModule } from 'ngx-quill';
 
 @NgModule({
   declarations: [
@@ -21,7 +35,29 @@ import { SharedModule } from 'src/app/shared/shared.module';
   imports: [
     CommonModule,
     OutrightDealRoutingModule,
-    SharedModule
+    SharedModule,
+     NgSelectModule,
+        NgbModule,  
+        // CKEditorModule,
+        AngularEditorModule,
+        HttpClientModule,
+        FormsModule,
+        ReactiveFormsModule,
+        ToastrModule.forRoot(),
+        // ArchwizardModule,
+        // NgWizardModule,
+        // AngularMultiSelectModule,
+        // NgxDropzoneModule,
+        DropzoneModule,
+        // NgxIntlTelInputModule,
+        // TreeviewModule.forRoot(),
+        HighlightModule,
+        NgxEditorModule,
+        DropzoneModule,
+        NgxMatIntlTelInputComponent,
+        MaterialModuleModule,
+        QuillModule,
+        // HttpClientModule
   ]
 })
 export class OutrightDealModule { }
